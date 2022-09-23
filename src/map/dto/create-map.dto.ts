1dd8b8528e327d638712b2d3fa65e457c1f57475
swapnil-mirgane <jigarswapnil143@gmail.com>
2022-09-23 10:05:52 +0530

    polygoan add working fine

--- src/map/dto/create-map.dto.ts
+++ src/map/dto/create-map.dto.ts
@@ -5,3 +5,14 @@ export class CreateMapDto {
     Name:string;
     City_Name:string
 }
+// import { IsOptional } from "class-validator";
+// import { Polygon, Position } from "geojson";
+
+// export class CreateParcelPointDto { 
+//     @IsOptional()
+//     position?:number[][]
+//     polygon?: Polygon;
+//     //createParcelPointDto: Position[];
+
+    
+// }
